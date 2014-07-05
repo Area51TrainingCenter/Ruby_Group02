7fb07ce464f6cfcbb78cf065a82b14ccf329dd82
Alvaro Pereyra <alvaro@xendacentral.com>
2014-07-05 06:54:40 -0500

    Adds default action. Class Method. Method Missing

--- module01/class04/controllers/facebook_controller.rb
+++ module01/class04/controllers/facebook_controller.rb
@@ -1,14 +1,13 @@
 class FacebookController < Controller
+
+  default_action :index
+
   def index(args = {})
     render "index", name: "Alvaro"
   end
 
   def new(args = {})
     render "index", name: "Alvaro"
-  end
-
-  def method_missing(name, *args)
-    index(args)
   end
 
 end
--- module01/class04/lib/nails/controller.rb
+++ module01/class04/lib/nails/controller.rb
@@ -3,4 +3,27 @@ class Controller
     controller_name = self.class.name.gsub("Controller", "").downcase
     View.render controller_name, action, params
   end
+
+  def default_action
+    self.class.default_action
+  end
+
+  class << self
+
+    attr_accessor :default_action
+
+    def default_action(name = :index)
+      @default_action = name
+    end
+
+  end
+
+  def method_missing(name, *args)
+    if default_action
+      self.send(default_action, args)
+    else
+      super
+    end
+  end
+
 end
